d60a02a04509bb7c9786d1aeba7ec94ef99d4006
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2022-05-06 12:20:31 +0900

    Add a full_title helpeer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_ttl(page_ttl = '')
+    base_ttl = "Ruby on Rails Tutorial Sample App"
+    if page_ttl.empty?
+      base_ttl
+    else 
+      page_ttl + " | " + base_ttl
+    end
+  end
+  def sample_ttl(page_ttl = '') 
+    base_ttl = "hoghoge on Rails Tutorial Sample App"
+    if page_ttl.empty?
+      base_ttl
+    else
+      page_ttl + " | " + base_ttl
+    end
+  end
 end
